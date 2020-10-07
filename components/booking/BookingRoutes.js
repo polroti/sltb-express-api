f223cd99380f0fe8bbd77285b54c320226047818
Manoj Kumar <dharmakumar338@gmail.com>
2020-10-07 21:01:39 +0530

    added mongodb connection, booking route to index, and secrets.json to index and added BookingRoute.js and relevant changes in addbooking.js

---
+++ components/booking/BookingRoutes.js
@@ -0,0 +1,16 @@
+/* 
+   ====================== PLEASE ADD YOUR CHANGE DESCRIPTIONS HERE ==============
+    07-10-2020 - File created. addBooking route
+
+*/
+const express = require('express')
+const router = express.Router()
+
+//CRUD METHODS
+const addBooking = require('../booking/Methods/addBooking')
+
+// Add a new booking
+router.post('/add', addBooking.AddBooking);
+
+
+module.exports = router;
