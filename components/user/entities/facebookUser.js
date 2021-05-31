4c907994a522b7354d0f74bcea9cdf490d1b2148
Manoj Kumar <dharmakumar338@gmail.com>
2021-05-31 18:59:15 +0530

    facebook user, google user

--- components/user/entities/facebookUser.js
+++ components/user/entities/facebookUser.js
@@ -1,7 +1,7 @@
 /* 
    ====================== PLEASE ADD YOUR CHANGE DESCRIPTIONS HERE ==============
     21-01-2021 - Manoj -  created the file, added username, fullname, password
-    31-05-2021 - Manoj -  added new fields:
+    31-05-2021 - Manoj -  added new fields: facebookuid, userid
 */
 
 const mongoose = require("mongoose");
