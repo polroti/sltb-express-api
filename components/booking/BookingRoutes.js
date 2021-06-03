e0a5f79d2b3d21408a8b06c47839624b7fa5cbd7
Manoj Kumar <dharmakumar338@gmail.com>
2021-06-03 22:10:03 +0530

    code refactor - booking rout

--- components/booking/BookingRoutes.js
+++ components/booking/BookingRoutes.js
@@ -3,14 +3,13 @@
     07-10-2020 - Manoj - File created. addBooking route
 
 */
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //CRUD METHODS
-const addBooking = require('../booking/Methods/addBooking')
+const addBooking = require("../booking/Methods/addBooking");
 
 // Add a new booking
-router.post('/add', addBooking.AddBooking);
-
+router.post("/add", addBooking.AddBooking);
 
 module.exports = router;
